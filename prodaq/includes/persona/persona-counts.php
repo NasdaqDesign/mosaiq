effca2246a8d8c027564c42397204fbc28c1fd74
Jason A Delia <Jason.Delia@nasdaq.com>
2016-05-12 15:24:50 -0400

    Removing unused post types from dashboard documenation

--- prodaq/includes/persona/persona-counts.php
+++ prodaq/includes/persona/persona-counts.php
@@ -13,53 +13,52 @@ $very_experienced = 0;
 $other_personas = array();
 
 if(!empty($related_participants)){
+	foreach($related_participants as $participant){
+		$related = get_post($participant);
+		$region = get_post_meta($related->ID, '_participant_region', true);
+		$market = get_post_meta($related->ID, '_participant_market', true);
+		$experience = get_post_meta($related->ID, '_participant_experience', true);
+		$primary = get_post_meta($related->ID, '_participant_primary_persona', true);
+		$participantCount ++;
 
-  foreach($related_participants as $participant){
-    $related = get_post($participant);
-    $region = get_post_meta($related->ID, '_participant_region', true);
-    $market = get_post_meta($related->ID, '_participant_market', true);
-    $experience = get_post_meta($related->ID, '_participant_experience', true);
-    $primary = get_post_meta($related->ID, '_participant_primary_persona', true);
-    $participantCount ++;
-
-    if(!empty($primary) && $primary != $post->ID){
-      array_push($other_personas, $primary);
-    }
-    switch($experience){
-      case '4':
-        $not_experienced ++;
-        break;
-      case '5-10':
-        $experienced ++;
-        break;
-      case '11':
-        $very_experienced ++;
-        break;
-    }
-    switch($region){
-      case 'amers':
-        $amersCount ++;
-        break;
-      case 'emea':
-        $emeaCount ++;
-        break;
-      case 'apac':
-        $apacCount ++;
-        break;
-    }
-    switch($market){
-      case 'small':
-        $small ++;
-        break;
-      case 'medium':
-        $mid ++;
-        break;
-      case 'large':
-        $large ++;
-        break;
-      case 'mega':
-        $mega ++;
-        break;
-    }
-  }
+		if(!empty($primary) && $primary != $post->ID){
+		array_push($other_personas, $primary);
+		}
+		switch($experience){
+		case '4':
+			$not_experienced ++;
+			break;
+		case '5-10':
+			$experienced ++;
+			break;
+		case '11':
+			$very_experienced ++;
+			break;
+		}
+		switch($region){
+		case 'amers':
+			$amersCount ++;
+			break;
+		case 'emea':
+			$emeaCount ++;
+			break;
+		case 'apac':
+			$apacCount ++;
+			break;
+		}
+		switch($market){
+		case 'small':
+			$small ++;
+			break;
+		case 'medium':
+			$mid ++;
+			break;
+		case 'large':
+			$large ++;
+			break;
+		case 'mega':
+			$mega ++;
+			break;
+		}
+	}
 }
