a47ceab3206ff5b25dd7d6c288ef4ff1732d890e
Jason A Delia <Jason.Delia@nasdaq.com>
2016-05-16 14:53:45 -0400

    Moving markup inside of the check so we don't have empty dl's on the page

--- prodaq/includes/participant/participant-details.php
+++ prodaq/includes/participant/participant-details.php
@@ -18,55 +18,54 @@
 	 }?>
 
 </ul>
-<dl>
 <?php if(!empty($company_industry)){
-	echo '<dt>Industry</dt>';
-		$industryJson = file_get_contents(get_template_directory() . '/library/data/industry.json');
-		$industryArr = json_decode($industryJson, true);
-		$industries = $industryArr[industryCodes];
-		foreach($industries as $industry){
-			if($industry[code] == $company_industry){
-				echo '<dd>' . $industry['description'] . '</dd>';
+	echo '<dl>';
+		echo '<dt>Industry</dt>';
+			$industryJson = file_get_contents(get_template_directory() . '/library/data/industry.json');
+			$industryArr = json_decode($industryJson, true);
+			$industries = $industryArr[industryCodes];
+			foreach($industries as $industry){
+				if($industry[code] == $company_industry){
+					echo '<dd>' . $industry['description'] . '</dd>';
+				}
 			}
-		}
+	echo '</dl>';
 } ?>
-</dl>
-<dl>
 <?php if(!empty($company_size)){
-	echo '<dt>Company Size</dt>';
-		$sizeJson = file_get_contents(get_template_directory() . '/library/data/company-size.json');
-		$sizeArr = json_decode($sizeJson, true);
-		$sizes = $sizeArr[companySizes];
-		foreach($sizes as $size){
-			if($size[code] == $company_size){
-				echo '<dd>' . $size['description'] . '</dd>';
+	echo '<dl>';
+		echo '<dt>Company Size</dt>';
+			$sizeJson = file_get_contents(get_template_directory() . '/library/data/company-size.json');
+			$sizeArr = json_decode($sizeJson, true);
+			$sizes = $sizeArr[companySizes];
+			foreach($sizes as $size){
+				if($size[code] == $company_size){
+					echo '<dd>' . $size['description'] . '</dd>';
+				}
 			}
-		}
+	echo '</dl>';
 } ?>
-</dl>
-<dl>
 <?php if(!empty($company_market)){
-	echo '<dt>Market Cap</dt>';
-	echo '<dd>' . strtoupper($company_market) . '</dd>';
+	echo '<dl>';
+		echo '<dt>Market Cap</dt>';
+		echo '<dd>' . strtoupper($company_market) . '</dd>';
+	echo '</dl>';
 } ?>
-</dl>
-<dl>
-	<?php if(!empty($city) || !empty($state) || !empty($country)){
+<?php if(!empty($city) || !empty($state) || !empty($country)){
+	echo '<dl>';
 		echo '<dt>Location</dt>';
-	}
-	echo '<dd>';
-		if(!empty($city)){
-			echo $city . ', ';
-		}
-		if(!empty($state)){
-			echo $state . ' ';
-		}
-		if(!empty($country)){
-			echo $country;
-		}
-	echo '</dd>';?>
-
-</dl>
+		echo '<dd>';
+			if(!empty($city)){
+				echo $city . ', ';
+			}
+			if(!empty($state)){
+				echo $state . ' ';
+			}
+			if(!empty($country)){
+				echo $country;
+			}
+		echo '</dd>';
+	echo '</dl>';
+} ?>
 <?php if(isset($region)){
 	echo '<dl>
 		<dt>Region</dt>
