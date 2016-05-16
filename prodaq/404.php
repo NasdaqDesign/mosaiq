36fcfdd7ac1c78bca06bc44eddbc02a6dddb0d9c
Jason A Delia <Jason.Delia@nasdaq.com>
2016-05-16 13:19:31 -0400

    Updating default 404 page

--- prodaq/404.php
+++ prodaq/404.php
@@ -1,34 +1,27 @@
 <?php get_header(); ?>
 
-      <div class="container">
+<div>
 
-        <div id="content">
-
-          <div id="main" class="col-md-8 col-md-offset-2 clearfix" role="main">
-
-            <article id="post-not-found" class="hentry clearfix">
-
-              <header class="article-header text-center">
-
-                <h1>404. Whelp...This is embarrassing...</h1>
-                <img src="<?php bloginfo('template_directory'); ?>/library/images/hal.jpg" />
-                <p>Look, I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill, and think things over. </p>
-
-              </header> <?php // end article header ?>
-
-              <section class="entry-content">
-
-                <p><?php get_search_form(); ?></p>
-
-              </section> <?php // end article section ?>
-
-            </article> <?php // end article ?>
-
-          </div> <?php // end #main ?>
-
-        </div> <?php // end #content ?>
-
-      </div> <?php // end ./container ?>
-
+  <header class="page__header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>Page Not Found</h1>
+        </div>
+      </div>
+  </header>
+  <section class="error__content">
+    <div class="container">
+      <h2> Sorry About This</h2>
+      <div class="row">
+        <div class="col-md-12">
+          <p>Perhaps you were looking for a listing of <a href="#">campaigns</a> or <a href="#">personas</a>? Maybe you were trying to get to the <a href="#">stats</a> page?</p>
+          <p>If you were trying to find out the difference between UI and UX we can help with that.</p>
+          <iframe src="http://prodaqo.mx/uivsux.php" class="uivsux"></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
 
 <?php get_footer(); ?>
